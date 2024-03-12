fbd940ff0d774794fd7492a0751e0086e5c5499f
J Lee <44005960+johndev86@users.noreply.github.com>
2024-03-12 10:57:59 -0400

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
@@ -29,12 +30,27 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")] // /api/users/2
+        [HttpGet("{username}")] // /api/users/liliana
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             var user = await _repository.GetMemberAsync(username);
 
             return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var user = await _repository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if (await _repository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
